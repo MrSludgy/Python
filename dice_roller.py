061fa651e7421071050e77d81285bad9c557d358
MrSludgy <MrSludgy@users.noreply.github.com>
2017-11-09 20:54:32 -0500

    Added Function Call
    Added function call to dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,16 +5,21 @@ import re
 from random import randint
 
 length=len(sys.argv)
-for i in range(1, length):
-    diceArg = re.findall('\d+', sys.argv[i])
-    diceCount = int(diceArg[0])
-    diceRange = int(diceArg[1])
-    for j in range(0, diceCount):
-      if diceRange>20:
-          print('Dice has too many sides. Please try again.')
-      elif diceRange%2 == 0:
-        print('%d' % randint(1, diceRange))
-      else:
-        print('%d'% randint(1, diceRange-1))
-      if j == diceCount-1:
-        print()
+
+def main():
+  for i in range(1, length):
+      diceArg = re.findall('\d+', sys.argv[i])
+      diceCount = int(diceArg[0])
+      diceRange = int(diceArg[1])
+      for j in range(0, diceCount):
+        #if diceRange>20:
+            #print('Dice has too many sides. Please try again.')
+        if diceRange%2 == 0:
+          print('%d' % randint(1, diceRange))
+        else:
+          print('%d'% randint(1, diceRange-1))
+        if j == diceCount-1:
+          print()
+
+if sys.argv[0] == 'dice_roller.py':
+  main()
